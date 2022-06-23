973af4c14dac046b0408e996332d7055fbe978b5
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2022-06-23 15:09:16 +0200

    delete item

--- src/app/components/tree-item/tree-item.component.ts
+++ src/app/components/tree-item/tree-item.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TreeNode } from 'src/app/interfaces/tree-node';
 
 @Component({
   selector: 'app-tree-item',
   templateUrl: './tree-item.component.html',
-  styleUrls: ['./tree-item.component.css']
+  styleUrls: ['./tree-item.component.css'],
 })
 export class TreeItemComponent implements OnInit {
   @Input() nodeData?: TreeNode;
+  @Output() deleteMe: EventEmitter<TreeNode> = new EventEmitter<TreeNode>();
   closed: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  get isFolder():boolean {
-    return Array.isArray(this.nodeData?.items)
+  get isFolder(): boolean {
+    return Array.isArray(this.nodeData?.items);
   }
 
   toggleNode(): void {
@@ -24,4 +24,15 @@ export class TreeItemComponent implements OnInit {
     this.closed = !this.closed;
   }
 
+  clickDelete(event: Event): void {
+    event.stopPropagation();
+    this.deleteMe.emit(this.nodeData);
+  }
+
+  deleteItem(nodeData: TreeNode): void {
+    console.log(this.nodeData?.label, 'törli ->', nodeData.label);
+    this.nodeData!.items = this.nodeData!.items!.filter(
+      (item) => item !== nodeData
+    );
+  }
 }
